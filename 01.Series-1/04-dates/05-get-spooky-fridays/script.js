0073ff0b5d703d518ba66f9da1c69fc8477f2e64
John <john.laterre@gmail.com>
2020-07-09 22:07:16 +0200

    04-dates

--- 01.Series-1/04-dates/05-get-spooky-fridays/script.js
+++ 01.Series-1/04-dates/05-get-spooky-fridays/script.js
@@ -6,4 +6,18 @@
 
     // your code here
 
+    document.getElementById("run").addEventListener("click", function () {
+        var year = document.getElementById("year").value;
+        var counter = [];
+
+        for (let month = 0; month < 12; month++) {
+            let date = new Date(year, month, 13);
+            let monthName = date.toLocaleString("en-US", { month: "long" })
+            if (date.getDay() == 5) {
+                counter.push(monthName);
+            }
+        }
+        alert(counter);
+    })
+
 })();

d5f57ce51fef50adf03d3072f69a76c61b549ccc
John <john.laterre@gmail.com>
2020-07-09 14:56:05 +0200

    03-colors

---
+++ 01.Series-1/03-colors/02-change-bcg-two/script.js
@@ -0,0 +1,12 @@
+
+// 03-colors/02-change-bcg-two/script.js - 3.2: Bcg 2
+
+(() => {
+
+    // your code here
+
+    document.getElementById("run").addEventListener("click", function () {
+        document.body.style.backgroundColor = document.getElementById("color").value;
+    })
+
+})();
---
+++ 01.Series-1/03-colors/03-change-bcg-three/script.js
@@ -0,0 +1,13 @@
+
+// 03-colors/03-change-bcg-three/script.js - 3.3: background color (3)
+
+
+(() => {
+
+    // your code here
+
+    document.getElementById("run").addEventListener("click", function () {
+        document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    })
+
+})();

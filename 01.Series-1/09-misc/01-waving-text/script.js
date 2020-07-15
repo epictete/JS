3a3c83cd06ed61b1ab991f95e65887c0f99a5358
John <john.laterre@gmail.com>
2020-07-15 22:26:31 +0200

    09-01 update

--- 01.Series-1/09-misc/01-waving-text/script.js
+++ 01.Series-1/09-misc/01-waving-text/script.js
@@ -5,28 +5,44 @@
 
     // your code here
 
-    let target = document.getElementById("target");
-    let string = target.textContent;
+    const target = document.getElementById("target");
+    const string = target.textContent;
+    let i = 0;
+    let forward = true;
 
-    for (let i = 0; i < string.length; i++) {
-        task(i);
+    setInterval(move, 100);
+
+    function move() {
+        if (forward === true) {
+            if (i <= string.length) {
+                wave();
+                i++;
+            } else {
+                forward = false;
+            }
+        } else {
+            if (i > -10) {
+                wave();
+                i--;
+            } else {
+                forward = true;
+            }
+        }
     }
 
-    function task(i) {
-        setTimeout(function () {
-            let newString = string.substring(0, i)
-                + '<span style="font-size: 1.3em">' + string.substring(i, i + 1) + '</span>'
-                + '<span style="font-size: 1.6em">' + string.substring(i + 1, i + 2) + '</span>'
-                + '<span style="font-size: 1.9em">' + string.substring(i + 2, i + 3) + '</span>'
-                + '<span style="font-size: 2.2em">' + string.substring(i + 3, i + 4) + '</span>'
-                + '<span style="font-size: 2.5em">' + string.substring(i + 4, i + 5) + '</span>'
-                + '<span style="font-size: 2.2em">' + string.substring(i + 5, i + 6) + '</span>'
-                + '<span style="font-size: 1.9em">' + string.substring(i + 6, i + 7) + '</span>'
-                + '<span style="font-size: 1.6em">' + string.substring(i + 7, i + 8) + '</span>'
-                + '<span style="font-size: 1.3em">' + string.substring(i + 8, i + 9) + '</span>'
-                + string.substring(i + 10, string.length);
-            target.innerHTML = newString;
-        }, 100 * i);
+    function wave() {
+        let newString = string.substring(0, i)
+            + '<span style="font-size: 1.3em">' + string.substring(i, i + 1) + '</span>'
+            + '<span style="font-size: 1.6em">' + string.substring(i + 1, i + 2) + '</span>'
+            + '<span style="font-size: 1.9em">' + string.substring(i + 2, i + 3) + '</span>'
+            + '<span style="font-size: 2.2em">' + string.substring(i + 3, i + 4) + '</span>'
+            + '<span style="font-size: 2.5em">' + string.substring(i + 4, i + 5) + '</span>'
+            + '<span style="font-size: 2.2em">' + string.substring(i + 5, i + 6) + '</span>'
+            + '<span style="font-size: 1.9em">' + string.substring(i + 6, i + 7) + '</span>'
+            + '<span style="font-size: 1.6em">' + string.substring(i + 7, i + 8) + '</span>'
+            + '<span style="font-size: 1.3em">' + string.substring(i + 8, i + 9) + '</span>'
+            + string.substring(i + 9);
+        target.innerHTML = newString;
     }
 
 })();
--- 01.Series-1/09-misc/04-worst-ui-two/script.js
+++ 01.Series-1/09-misc/04-worst-ui-two/script.js
@@ -5,75 +5,101 @@
 
     // your code here
 
-    let target = document.getElementById("target");
-    let string = target.innerText;
+    document.getElementById("part-one").addEventListener("click", updateUI);
+    document.getElementById("part-two").addEventListener("click", updateUI);
+    document.getElementById("part-three").addEventListener("click", updateUI);
+    document.getElementById("part-four").addEventListener("click", updateUI);
+
     function zero(num) {
         return ("0" + num).slice(-2);
     }
 
-    // One
+    function updateUI() {
+        let target = document.getElementById("target");
+        let current = event.currentTarget
+        let currentID = current.getAttribute("id");
+        let min = parseInt(event.currentTarget.getAttribute("data-min"), 10);
+        let max = parseInt(event.currentTarget.getAttribute("data-max"), 10);
+        let currentValue = min;
 
-    let one = document.getElementById("part-one");
-    let oneMin = parseInt(one.getAttribute("data-min"), 10);
-    let oneMax = parseInt(one.getAttribute("data-max"), 10);
-    let newOne = oneMin;
-
-    one.addEventListener("click", () => {
-        if (newOne < oneMax) {
-            newOne += 1;
+        if (currentValue <= max) {
+            currentValue++;
         }
-        let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
-        target.innerHTML = newString;
-        one.innerHTML = newOne;
-    })
+        current.innerHTML = zero(currentValue);
+    }
 
-    // Two
-
-    let two = document.getElementById("part-two");
-    let twoMin = parseInt(two.getAttribute("data-min"), 10);
-    let twoMax = parseInt(two.getAttribute("data-max"), 10);
-    let newTwo = twoMin;
-
-    two.addEventListener("click", () => {
-        console.log(event.currentTarget);
-        if (newTwo < twoMax) {
-            newTwo += 1;
+    /*
+    
+        let target = document.getElementById("target");
+    
+        function zero(num) {
+            return ("0" + num).slice(-2);
         }
-        let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
-        target.innerHTML = newString;
-        two.innerHTML = zero(newTwo);
-    })
-
-    // Three
-
-    let three = document.getElementById("part-three");
-    let threeMin = parseInt(three.getAttribute("data-min"), 10);
-    let threeMax = parseInt(three.getAttribute("data-max"), 10);
-    let newThree = threeMin;
-
-    three.addEventListener("click", () => {
-        if (newThree < threeMax) {
-            newThree += 1;
-        }
-        let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
-        target.innerHTML = newString;
-        three.innerHTML = zero(newThree);
-    })
-
-    // Four
-
-    let four = document.getElementById("part-four");
-    let fourMin = parseInt(four.getAttribute("data-min"), 10);
-    let fourMax = parseInt(four.getAttribute("data-max"), 10);
-    let newFour = fourMin;
-
-    four.addEventListener("click", () => {
-        if (newFour < fourMax) {
-            newFour += 1;
-        }
-        let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
-        target.innerHTML = newString;
-        four.innerHTML = zero(newFour);
-    })
+    
+        // One
+    
+        let one = document.getElementById("part-one");
+        let oneMin = parseInt(one.getAttribute("data-min"), 10);
+        let oneMax = parseInt(one.getAttribute("data-max"), 10);
+        let newOne = oneMin;
+    
+        one.addEventListener("click", () => {
+            if (newOne < oneMax) {
+                newOne += 1;
+            }
+            let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
+            target.innerHTML = newString;
+            one.innerHTML = newOne;
+        })
+    
+        // Two
+    
+        let two = document.getElementById("part-two");
+        let twoMin = parseInt(two.getAttribute("data-min"), 10);
+        let twoMax = parseInt(two.getAttribute("data-max"), 10);
+        let newTwo = twoMin;
+    
+        two.addEventListener("click", () => {
+            if (newTwo < twoMax) {
+                newTwo += 1;
+            }
+            let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
+            target.innerHTML = newString;
+            two.innerHTML = zero(newTwo);
+        })
+    
+        // Three
+    
+        let three = document.getElementById("part-three");
+        let threeMin = parseInt(three.getAttribute("data-min"), 10);
+        let threeMax = parseInt(three.getAttribute("data-max"), 10);
+        let newThree = threeMin;
+    
+        three.addEventListener("click", () => {
+            if (newThree < threeMax) {
+                newThree += 1;
+            }
+            let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
+            target.innerHTML = newString;
+            three.innerHTML = zero(newThree);
+        })
+    
+        // Four
+    
+        let four = document.getElementById("part-four");
+        let fourMin = parseInt(four.getAttribute("data-min"), 10);
+        let fourMax = parseInt(four.getAttribute("data-max"), 10);
+        let newFour = fourMin;
+    
+        four.addEventListener("click", () => {
+            if (newFour < fourMax) {
+                newFour += 1;
+            }
+            let newString = "+" + newOne + zero(newTwo) + zero(newThree) + zero(newFour);
+            target.innerHTML = newString;
+            four.innerHTML = zero(newFour);
+        })
+    
+    */
 
 })();
